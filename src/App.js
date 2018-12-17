6f267cdcc86d78322d1ba861e4453eb1a1811690
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2018-12-17 11:38:19 +0600

    add button and circle for render numbers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Render from "./components/RenderComponent/Render";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        numbers: [Math.floor(Math.random() * 36 + 1)]
+    };
+
+    changeNumber = () => {
+
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="render">
+                    <div>
+                        <button onClick={this.changeNumber}>New numbers</button>
+                        <Render></Render>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/RenderComponent/Render.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+import './Render.css'
+
+class RenderContent extends Component {
+    render() {
+        return (
+            <div className="renderNumber">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+            </div>
+        )
+    }
+
+}
+
+export default RenderContent;
